2566eafc05ebf206bbfb665e539f3da8d72435d6
moshe-nahar <nahar@miriaminfleuere.com>
2018-06-03 17:51:05 +0300

    btstrp

--- index.php
+++ index.php
@@ -38,6 +38,21 @@
 </div>
 
 <div class="container">
+    <h1>OurPackages</h1>
+    <div class="row">
+        <div class="col-sm-4" style="background-color:yellow;">
+            <p>Lorem ipsum...</p>
+        </div>
+        <div class="col-sm-4" style="background-color:pink;">
+            <p>Sed ut perspiciatis...</p>
+        </div>
+        <div class="col-sm-4" style="background-color:beige;">
+            <p>Ut piciatis...</p>
+        </div>        
+    </div>
+</div>
+
+<div class="container">
     <h3>Home Page</h3>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 </div>
